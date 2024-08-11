f5e76efb13a1b911fa7889a3f338355699ac658e
Ziad Ibrahim <134080590+ZiadTantawy@users.noreply.github.com>
2024-08-11 12:44:15 +0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home';
 import Navbar from './navbar';
 import Login from './login';
+import EmployeeOfTheMonthPage from './EmployeeOfTheMonth';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/login" element={<Login/>}/>
+        <Route path="/nomination" element={EmployeeOfTheMonthPage}/>
       </Routes>
     </Router>
   );
