770842181ffaa4c27f3488090bd996d92ad9d777
Ziad Ibrahim <134080590+ZiadTantawy@users.noreply.github.com>
2024-08-11 11:45:35 +0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import Navbar from './navbar';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './home';
+import LoginPage from './LoginPage';
+
 function App() {
   return (
-    <div>
-      <Navbar/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
   );
 }
 
