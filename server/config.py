9e5ed49e517fbe82dcb0dd4475f91c9a1d62252b
Ziad Ibrahim <134080590+ZiadTantawy@users.noreply.github.com>
2024-08-16 19:58:32 +0300

    .

--- server/config.py
+++ server/config.py
@@ -1,9 +1,9 @@
 DATABASE = {
     'dbname': 'itworx',
     'user': 'postgres',
-    'password': '123',
+    'password': '1234',
     'host': 'localhost'
 }
 
 CORS_ORIGINS = ["http://localhost:3000"]
-SECRET_KEY = "123"
+SECRET_KEY = "1234"
